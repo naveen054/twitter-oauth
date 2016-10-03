c8ea2846dbeeb4f5804d2fda369a3fc1218c9381
naveen <naveenkerati@gmail.com>
2016-10-03 06:47:59 +0530

    `

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var config = require('_config');
+var twt = new require('node-twitter-api')(config.twitter);
 
+app.use(express.static(__dirname+'/web/views'));
+app.use(express.static(__dirname+'/web/js'));
+app.use(express.static(__dirname+'/web/css'));
 app.get('/',function(req,res){
-
-  res.sendFile(path.join(__dirname,'index.html'));
-
-
+  res.render('index');
 });
-app.listen(1234);
+app.get('/auth',function(req,res){
+  twt.getRequestToken(function(err,oAuthtoken,oauthTokenSecret,results){
+    if(err){
+      console.log(err);
+    }else {
+      console.log(oAuthtoken);
+    }
+  })
+})
+app.listen(config.port);
